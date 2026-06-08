b4c806391c451f16151e9fea878168f0c4e2359b
unknown <himanshuprofile1@gmail.com>
2026-06-08 12:32:19 +0530

    Day 1 completed - stock data retrieval working

--- src/stock_data.py
+++ src/stock_data.py
@@ -5,8 +5,9 @@ ticker = yf.Ticker(company)
 info = ticker.info
 
 def save_company_data (company):
+    print (f"Downloading data for {company}")
     df = yf.download(company, period="1mo")
-    df.to_csv(f"data/{info.get("longName")}.csv", index=False)
-
+    df.to_csv(f"data/{company}.csv", index=False)
+    print (f"Downloaded data for {company}")
 
 save_company_data(company)
